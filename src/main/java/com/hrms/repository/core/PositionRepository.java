6382ecca4e17e4cc317735c8896e8da42243de8b
kunalprajapat854 <kunalprajapat221@gmail.com>
2025-06-19 19:39:01 +0530

    change the datatype of integer to long

--- src/main/java/com/hrms/repository/core/PositionRepository.java
+++ src/main/java/com/hrms/repository/core/PositionRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hrms.entities.EmployeePosition;
+import com.hrms.entities.Position;
 
 @Repository
-public interface PositionRepository extends JpaRepository<EmployeePosition, Integer> {
+public interface PositionRepository extends JpaRepository<Position, Long> {
 
 }
