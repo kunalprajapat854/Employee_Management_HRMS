53df1ce4d113adb44b9a031512a2e92dd8620d65
kunalprajapat854 <kunalprajapat221@gmail.com>
2025-06-27 00:01:38 +0530

    create findBy methods

--- src/main/java/com/hrms/repository/core/attendence/AttendenceRepository.java
+++ src/main/java/com/hrms/repository/core/attendence/AttendenceRepository.java
@@ -1,10 +1,22 @@
 package com.hrms.repository.core.attendence;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hrms.entities.Attendence;
-@Repository
-public interface AttendenceRepository  extends JpaRepository<Attendence, Long>{ 
 
+@Repository
+public interface AttendenceRepository extends JpaRepository<Attendence, Long> {
+
+	public List<Attendence> findByEmployeeId(Long employeeId);
+
+	public Optional<Attendence> findByEmployeeIdAndDate(Long employeeId, LocalDate date);
+
+	public List<Attendence> findByEmployeeIdAndDateBetween(Long employeeId, LocalDate from, LocalDate to);
+
+	public List<Attendence> findByDate(LocalDate date);
 }
