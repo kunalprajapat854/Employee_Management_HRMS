08f2eb817b084925551c5a17bc2cde462eb52ca4
kunalprajapat854 <kunalprajapat221@gmail.com>
2025-06-26 23:03:03 +0530

    department interface and service class

--- src/main/java/com/hrms/service/attendence/AttendenceServiceImple.java
+++ src/main/java/com/hrms/service/attendence/AttendenceServiceImple.java
@@ -1,8 +1,9 @@
-package com.hrms.service.attendence;
+package com.hrms.service.attendeence;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -19,54 +20,48 @@ public class AttendenceServiceImple implements AttendenceService {
 		this.attendenceRepository = attendenceRepository;
 	}
 
-	
 	public void markAttendence(Long employeeId, LocalDate date, Attendence attendencedate) {
 
-
 	}
-
 
 	public List<Attendence> getAttendenceByEmployee(Long employeeId) {
+		return attendenceRepository.findAll();
 
-		return null;
 	}
-
 
 	public List<Attendence> getAttendencesBetweenDates(Long employeeId, LocalDate fromDate, LocalDate toDate) {
 
 		return null;
 	}
 
-
 	public boolean updateAttendence(Long employeeId, Attendence updateAttendence) {
-
+		Optional<Attendence> empId = attendenceRepository.findById(employeeId);
+		if(empId.isPresent()) {
+			Attendence attendence = empId.get();
+		    
+		}
 		return false;
 	}
-
 
 	public String deleteAttendence(Long attendenceId) {
 
 		return null;
 	}
 
-
 	public Attendence getTodayAttendence(Long employeeId) {
 
 		return null;
 	}
-
 
 	public BigDecimal calculateMonthlyWorkhours(Long employeeId, int year, int month) {
 
 		return null;
 	}
 
-
 	public List<Long> getListOfEmployeeAbsents(LocalDate date) {
 
 		return null;
 	}
-
 
 	public BigDecimal calculateOvertimeInRange(Long employeeId, LocalDate from, LocalDate to) {
 
